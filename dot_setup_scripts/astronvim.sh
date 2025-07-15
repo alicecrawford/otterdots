102dca7c45fc5116d7741ca18d83de6b35402069
Alice Lutris <alice@tuxnami.org>
2025-07-14 20:43:10 -0700

    Fixing astronvim setup

--- dot_setup_scripts/astronvim.sh
+++ dot_setup_scripts/astronvim.sh
@@ -1,1 +1,8 @@
-git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim\nrm -rf ~/.config/nvim/.git\nnvim
+mv ~/.config/nvim ~/.config/nvim.bak
+cp -R ~/.local/share/nvim ~/.local/share/nvim.bak
+cp -R ~/.local/state/nvim ~/.local/state/nvim.bak
+rm -rf ~/.local/share/nvim ~/.local/share/nvim
+mv ~/.cache/nvim ~/.cache/nvim.bak
+git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
+# remove template's git connection to set up your own later
+rm -rf ~/.config/nvim/.git
