77d38e305002d34337db23f6f2aff1cad3eba37b
Alice Elizabeth Zoe Lutris <451394+alicecrawford@users.noreply.github.com>
2026-02-06 11:45:58 -0800

    add linux to brew config

--- dot_zshenv.d/00-brew.zsh
+++ dot_zshenv.d/00-brew.zsh
@@ -3,3 +3,9 @@ if [[ "`uname`" == "Darwin" ]]; then
     eval $(/opt/homebrew/bin/brew shellenv)
   fi
 fi
+
+if [[ "`uname`" == "Linux" ]]; then
+  if [ -f /home/linuxbrew/.linuxbrew/bin/brew ]; then
+    eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  fi
+fi
